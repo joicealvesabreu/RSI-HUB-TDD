a5ddb4743119f8a369ca1269366e39849355f9ae
joicealvesabreu <54195636+joicealvesabreu@users.noreply.github.com>
2020-01-10 13:43:41 -0300

    Alterando tudo na classes

---
+++ src/main/java/br/com/rsinet/Hub_TDD/CadastrarCliente/HomePage.java
@@ -0,0 +1,108 @@
+package br.com.rsinet.Hub_TDD.CadastrarCliente;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+
+	private static WebElement element;
+
+	public static WebElement clicarmenu(WebDriver driver) {
+		element = driver.findElement(By.id("menuUserSVGPath"));
+		return element;
+	}
+
+	public static WebElement clicarCadastrar(WebDriver driver) {
+		element = driver.findElement(By.xpath("//a[@class='create-new-account ng-scope']"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		JavascriptExecutor ex = (JavascriptExecutor) driver;
+
+		ex.executeScript("arguments[0].click();", element);
+		return element;
+	}
+
+	public static WebElement nomeUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("usernameRegisterPage"));
+		
+		return element;
+		
+	}
+
+	public static WebElement emailUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("emailRegisterPage"));
+		return element;
+	}
+
+	public static WebElement senhaUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("passwordRegisterPage"));
+		return element;
+	}
+
+	public static WebElement confirmasenhaUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("confirm_passwordRegisterPage"));
+		return element;
+	}
+
+	public static WebElement primeiroNome(WebDriver driver) {
+		element = driver.findElement(By.name("first_nameRegisterPage"));
+		return element;
+	}
+
+	public static WebElement ultimoNome(WebDriver driver) {
+		element = driver.findElement(By.name("last_nameRegisterPage"));
+		return element;
+	}
+
+	public static WebElement telefoneUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("phone_numberRegisterPage"));
+		return element;
+	}
+
+	public static WebElement paisUsuario(WebDriver driver) {
+		WebElement element = driver.findElement(By.name("countryListboxRegisterPage"));
+		new Select(element).selectByVisibleText("Brazil");
+		return element;
+	}
+
+	public static WebElement cidadeUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("cityRegisterPage"));
+		return element;
+	}
+
+	public static WebElement enderecoUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("addressRegisterPage"));
+		return element;
+	}
+
+	public static WebElement estadoUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("state_/_province_/_regionRegisterPage"));
+		return element;
+	}
+
+	public static WebElement cepUsuario(WebDriver driver) {
+		element = driver.findElement(By.name("postal_codeRegisterPage"));
+		return element;
+	}
+
+	public static WebElement aceitaTermos(WebDriver driver) {
+		element = driver.findElement(By.name("allowOffersPromotion"));
+		return element;
+	}
+
+	public static WebElement aceitatermos2(WebDriver driver) {
+		element = driver.findElement(By.name("i_agree"));
+		return element;
+	}
+
+	public static WebElement registraUsuario(WebDriver driver) {
+		element = driver.findElement(By.id("register_btnundefined"));
+		return element;
+	}
+
+}
---
+++ src/main/java/br/com/rsinet/Hub_TDD/CadastrarCliente/framework/PageCadastro.java
@@ -0,0 +1,63 @@
+package br.com.rsinet.Hub_TDD.CadastrarCliente.framework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import br.com.rsinet.Hub_TDD.CadastrarCliente.HomePage;
+
+
+public class PageCadastro {
+
+	static WebDriver driver;
+
+	@BeforeClass
+	public static void testes() {
+
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.get("https://www.advantageonlineshopping.com/#/");
+
+	}
+
+	@Test
+	public void Test() throws InterruptedException {
+
+
+		HomePage.clicarmenu(driver).click();
+		HomePage.clicarCadastrar(driver).click();
+		
+		HomePage.nomeUsuario(driver).sendKeys("joice Natalice");
+		HomePage.emailUsuario(driver).sendKeys("joice_natalice16@hotmail.com");
+		HomePage.senhaUsuario(driver).sendKeys("Natalice24");
+		HomePage.confirmasenhaUsuario(driver).sendKeys("Natalice24");
+		
+		HomePage.primeiroNome(driver).sendKeys("joice");
+		HomePage.ultimoNome(driver).sendKeys("abreu");
+		HomePage.telefoneUsuario(driver).sendKeys("11941707376");
+		HomePage.paisUsuario(driver);
+		
+		HomePage.cidadeUsuario(driver).sendKeys("Taboao da Serra");
+		HomePage.enderecoUsuario(driver).sendKeys("Rua Albano Leite");
+		HomePage.estadoUsuario(driver).sendKeys("Sao Paulo");
+		HomePage.cepUsuario(driver).sendKeys("06773050");
+		
+		HomePage.aceitaTermos(driver).isSelected();
+		HomePage.aceitatermos2(driver).isSelected();
+		HomePage.registraUsuario(driver).isSelected();
+	}
+
+	@AfterClass
+	public static void finalizar() {
+		driver.quit();
+
+	}
+
+}
